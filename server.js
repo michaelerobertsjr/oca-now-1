ca8accf883d81eac2af263ea64c452db7878dce9
Michael Roberts <michael@origincodeacademy.com>
2018-07-02 17:43:46 -0700

    updates the alias settings

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 let app = express();
 
-app.get('*', (req, res) => res.send('Hello World'));
+app.get('*', (req, res) => res.send(`Feel'n it`));
 
 module.exports = app;
